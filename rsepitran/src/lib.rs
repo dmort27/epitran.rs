8540ab2d8d9b7af9f4aad30d5184b0f7fe4266c3
David R. Mortensen <dmortens@cs.cmu.edu>
2025-07-08 14:46:21 +0200

    Removed of redundant imports

--- rsepitran/src/lib.rs
+++ rsepitran/src/lib.rs
@@ -1,11 +1,11 @@
 //! RSEpitran - Rust implementation of Epitran functionality
-//! 
+//!
 //! This crate provides text processing utilities for linguistic analysis,
 //! including tokenization and phonetic transliteration using weighted finite
 //! state transducers (wFSTs).
 
-pub mod tokenize;
 pub mod epitran;
+pub mod tokenize;
 
-pub use tokenize::{tokenize_by_whitespace, filter_by_symbols};
 pub use epitran::Epitran;
+pub use tokenize::{filter_by_symbols, tokenize_by_whitespace};
